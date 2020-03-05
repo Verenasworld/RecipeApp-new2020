7371f73a431ede6de801d4967cc01dd5bdce62da
Verenasworld <verena.felizidas@gmail.com>
2020-03-05 16:21:25 +0100

    add a date

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from '../recipe.model';
-import {hostReportError} from 'rxjs/internal-compatibility';
+
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
- recipes: Recipe[] = [
+  dateToday: number = Date.now();
+  recipes: Recipe[] = [
    new Recipe( 'a test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg'),
-   new Recipe( 'a test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg')
+    new Recipe( 'a test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg'),
  ];
  constructor() { }
 
