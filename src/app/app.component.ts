477478f196759bbe3b2eae7df7cdabe6bb9469b0
Verenasworld <verena.felizidas@gmail.com>
2020-03-09 09:33:27 +0100

    use Event Binding for navigation sls List % recipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RecipeApp-new2020';
+  loadedFeature = 'recipe';
+
+ onNavigate(feature: string) {
+  this.loadedFeature = feature;
+ }
 }
