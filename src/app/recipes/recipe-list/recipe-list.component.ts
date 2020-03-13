41ad6d9104fd02944a0001407b512258c7de20b0
Verenasworld <verena.felizidas@gmail.com>
2020-03-13 19:47:07 +0100

    change the Recipe data to the service and provides in the recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,10 +11,7 @@ import {Recipe} from '../recipe.model';
 
 export class RecipeListComponent implements OnInit {
   @Output()recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-   new Recipe( 'Strawberry Cake', 'this is simply recipe for you ', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg'),
-    new Recipe( 'a test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg'),
- ];
+  recipes: Recipe[];
  constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Recipe} from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+   private recipes: Recipe[] = [
+    new Recipe('Strawberry Cake', 'this is simply recipe for you ', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg'),
+    new Recipe( 'a test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg'),
+  ];
 
+   getRecipes() {
+    return  this.recipes.slice();
+   }
   constructor() { }
 }
