faaf745fa3dc0244371c8938e727acefb19b6291
Verenasworld <verena.felizidas@gmail.com>
2020-03-13 20:09:04 +0100

    change the long way over Output to use EventEmitter,  to manage Cross Component Communication with Service & Injection

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
    private recipes: Recipe[] = [
     new Recipe('Strawberry Cake', 'this is simply recipe for you ', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg'),
     new Recipe( 'a test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556__480.jpg'),
