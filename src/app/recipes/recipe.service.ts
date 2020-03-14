2bc0f2cad79d01ac1f77bfac5b27ae827be917b6
Verenasworld <verena.felizidas@gmail.com>
2020-03-14 22:41:22 +0100

    inject recipe service to pass ingredients to it ,

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -28,5 +28,7 @@ export class RecipeService {
    getRecipes() {
     return  this.recipes.slice();
    }
-  constructor() { }
+   addIngredientsToShoppingList(ingredients: Ingredient[]) {
+
+   }
 }
